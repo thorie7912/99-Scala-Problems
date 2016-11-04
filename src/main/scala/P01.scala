f038a199023cdbaf1b1f74e3a6f3e3abbe4121b4
Takahiro Horie <takahiro.horie@creditkarma.com>
2016-11-04 12:09:58 -0700

    Updated P01

--- src/main/scala/P01.scala
+++ src/main/scala/P01.scala
@@ -3,6 +3,7 @@ package org.p99.scala
 object P01 {
 
   // procedural-style, getting the last element in a list
+  // this is NOT an ideal solution in a functional programming paradigm
   def get_last_element_procedural[Type](list: List[Type]): Type = {
       var i = 0
       if (list.length == 0) {
@@ -17,23 +18,28 @@ object P01 {
   }
 
   // recursive-style, getting the last element in a list
+  // better solution, but Scala has more tools
   def get_last_element_recursive[Type](list: List[Type]): Type = {
     if (list.tail == Nil) {
-      /*return*/ list.head // the return keyword is optional
+      // Note: the return keyword should be avoided, see: https://tpolecat.github.io/2014/05/09/return.html
+      /*return*/ list.head
     } else {
-      /*return*/ get_last_element_recursive(list.tail) // the return keyword is optional
+      // Note: the return keyword should be avoided, see: https://tpolecat.github.io/2014/05/09/return.html
+      /*return*/ get_last_element_recursive(list.tail)
     }
   }
 
   // built-in, getting the last element in a list
   def get_last_element_builtin[Type](list: List[Type]): Type = {
-    /*return*/ list.last // the return keyword is optional
+    // Note: the return keyword should be avoided, see: https://tpolecat.github.io/2014/05/09/return.html
+    /*return*/ list.last
   }
 
   // recursive with match, getting the last element in a list
   def get_last_element_recursive_with_match[Type](list: List[Type]): Type = {
 
-    import scala.collection.immutable.::
+    // The following import is not required, because it is automatically added by the compiler
+    //import scala.collection.immutable.::
 
     // Perform a pattern match on the list to recursively find the last element.
     // Since all Lists are made up of a two components (head element and tail List),
@@ -44,14 +50,16 @@ object P01 {
       // Base case:
       // If list matches the pattern "head :: Nil" meaning a List with head element
       // and a Nil (empty) tail element, then return the head
-      case head :: Nil => /*return*/ head // the return keyword is optional
+      // Note: the return keyword should be avoided, see: https://tpolecat.github.io/2014/05/09/return.html
+      case head :: Nil => /*return*/ head
 
       // Recursive case:
       // Else if list matches the pattern "_ :: tail" meaning a List with any head element
       // and a tail element, then recurse to get last element of the tail.
       // The _ represents an unnamed wildcard; we could use a variable name "head" but we
       // don't need to use the variable later so a wildcard will suffice.
-      case _ :: tail => /*return*/ get_last_element_recursive_with_match(tail) // the return keyword is optional
+      // Note: the return keyword should be avoided, see: https://tpolecat.github.io/2014/05/09/return.html
+      case _ :: tail => /*return*/ get_last_element_recursive_with_match(tail)
 
       // Else in all other cases, treat as an error (e.g. empty list).
       // The _ represents an unnamed wildcard.
